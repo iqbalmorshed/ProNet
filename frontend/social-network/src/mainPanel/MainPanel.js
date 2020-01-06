aa12c3db39153f0ad949a8488e81807e4e3509b5
Iqbal Morshed <iqbal.morshed24@gmail.com>
2020-01-06 00:29:05 -0500

    made some basic design regarding mainPanel and posts

---
+++ frontend/social-network/src/mainPanel/MainPanel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+function MainPanel() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/">
+                    
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default MainPanel
---
+++ frontend/social-network/src/posts/PostPanel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PostPanel() {
+    return (
+        <div>
+            <PostList>
+                {posts}
+            </PostList>
+        </div>
+    )
+}
+
+export default PostPanel
